4d32dc619e2284c4454aae5a1c513559f1c61ef5
openopen <openopen114@gmail.com>
2019-03-04 20:29:35 +0800

    Remove initialValue

--- src/components/SettingArea/index.jsx
+++ src/components/SettingArea/index.jsx
@@ -62,21 +62,21 @@ class SettingArea extends Component {
           {/* # Table Schema */}
           <Form.Item label="# Table Schema">
             {getFieldDecorator("tableSchema", {
-               initialValue:`
-              [ID] [varchar](64) NOT NULL,
-              [NAME] [varchar](64) NULL,
-              [IS_SON] [decimal](1, 0) NULL,
-              [PARENTID] [varchar](64) NULL,
-              [CREATE_TIME] [varchar](64) NULL,
-              [CREATE_USER] [varchar](64) NULL,
-              [LAST_MODIFIED] [varchar](64) NULL,
-              [LAST_MODIFY_USER] [varchar](64) NULL,
-              [TS] [varchar](64) NULL,
-              [DR] [decimal](11, 0) NULL,
-              [BPM_STATE] [decimal](11, 0) NULL,
-              [TENANT_ID] [varchar](64) NULL,
-              [CODE] [varchar](255) NULL
-            `,
+            //    initialValue:`
+            //   [ID] [varchar](64) NOT NULL,
+            //   [NAME] [varchar](64) NULL,
+            //   [IS_SON] [decimal](1, 0) NULL,
+            //   [PARENTID] [varchar](64) NULL,
+            //   [CREATE_TIME] [varchar](64) NULL,
+            //   [CREATE_USER] [varchar](64) NULL,
+            //   [LAST_MODIFIED] [varchar](64) NULL,
+            //   [LAST_MODIFY_USER] [varchar](64) NULL,
+            //   [TS] [varchar](64) NULL,
+            //   [DR] [decimal](11, 0) NULL,
+            //   [BPM_STATE] [decimal](11, 0) NULL,
+            //   [TENANT_ID] [varchar](64) NULL,
+            //   [CODE] [varchar](255) NULL
+            // `,
               rules: [
                 { required: true, message: "Please input your Table Schema!" }
               ]
